82ad6a132a74b9e6673351725a45226adf9986d3
marsov <marsov@mignix.com>
2013-10-24 18:47:06 +0200

    Add bootstraping

--- Framework.php
+++ Framework.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace Yo;
+use Yo\Event\Manager;
+use Yo\Layout\Page;
+use SplClassLoader;
+
+require '../ClassLoader/SplClassLoader.php';
 
 /**
  * Class Framework
@@ -8,5 +13,44 @@ namespace Yo;
  */
 class Framework
 {
+    /**
+     * @var SplClassLoader
+     */
+    protected $_classLoader;
 
+    /**
+     * @var Bootstrap
+     */
+    protected $_bootstrap;
+
+    /**
+     * @return SplClassLoader
+     */
+    public function getClassLoader()
+    {
+        return $this->_classLoader;
+    }
+
+    public function __construct()
+    {
+        $this->_classLoader = new \SplClassLoader('Yo', __DIR__ ."/..");
+        $this->_classLoader->register();
+        $this->_bootstrap = new Bootstrap($this);
+    }
+
+    public function start()
+    {
+
+    }
+
+    public function run()
+    {
+        $layout = new Page();
+        echo $layout->render();
+    }
+
+    public function end()
+    {
+
+    }
 }
