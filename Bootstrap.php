956c4524b75de852f567a36eec6b262899a640e5
marsov <marsov@mignix.com>
2013-10-24 18:46:21 +0200

    Bootstrap listeners in directory Listeners

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,8 +1,40 @@
 <?php
 
 namespace Yo;
+use Yo\Event\Manager;
 
+/**
+ * Class Bootstrap
+ * @package Yo
+ */
+class Bootstrap
+{
+    protected $_framework;
 
-class Bootstrap {
+    public function __construct(Framework $yo)
+    {
+        $this->_framework = $yo;
+        $this->initListeners();
+    }
+
+    public function initListeners()
+    {
+        // init observers
+        $listeners = scandir(__DIR__."/Listeners");
+
+        foreach ($listeners as $listener) {
+            $t = array('.', '..');
+            if (!in_array($listener, $t)) {
+                $listener = rtrim($listener, ".php");
+                $this->_framework->getClassLoader()->loadClass("Yo\\Listeners\\". $listener);
+
+                if (class_exists("Yo\\Listeners\\". $listener)) {
+                    $listenerName = "Yo\\Listeners\\". $listener;
+                    $listenerObject = new $listenerName();
+                    Manager::getInstance()->addListener($listenerObject);
+                }
+            }
+        }
+    }
 
 }
